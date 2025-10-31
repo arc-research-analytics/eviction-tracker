d312e1504cd3b5920bd6cb263af9880e3a499d9a
arc-research-analytics <arc-research-analytics@users.noreply.github.com>
2025-10-31 20:35:37 +0000

    deploy: 6438e9dc1d8b33b9b53f3c86519770f8c2f1bf4c

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     supabase: {
-        url: 'https://lrvlwdcrzkurkiasyzzi.supabase.co',
-        anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxydmx3ZGNyemt1cmtpYXN5enppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2MDk0OTUsImV4cCI6MjA3MzE4NTQ5NX0.ORD9EHEflFFKiv6GJrnOQ2sGn4uoogYWrhiq_Tll9AY'
+        url: 'https://wakzcuklgwutdvyzptuv.supabase.co',
+        anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indha3pjdWtsZ3d1dGR2eXpwdHV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MDc5NDcsImV4cCI6MjA3MTE4Mzk0N30.SfJhyX3HM5HTkOkrl2U1rndLGo2wEBCQUKkJSQ-0ekc'
     },
     mapbox: {
         accessToken: 'pk.eyJ1Ijoid3dyaWdodDIxIiwiYSI6ImNtZmZweTc2ZjBnbGEyeHB3dmRqZGg4dHcifQ.AU-tVnuPpaoQP2icjuFqkg',
